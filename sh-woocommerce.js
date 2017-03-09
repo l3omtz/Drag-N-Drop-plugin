61c263bd4c4e7ab8abd71d0cd1b52d8cb465a23a
Leo Martinez <l3o@Leos-MacBook-Pro.local>
2017-03-09 11:10:44 -0600

    First working click add/remove

--- sh-woocommerce.js
+++ sh-woocommerce.js
@@ -21,15 +21,21 @@ function initSelect2(){
 function initAddToCartPlusMinus(){
 
 	//  Add class to where the click should add product to cart
-	$j(document).on( 'click', '.quantity .plus, .quantity .minus, .product-thumbnail .img-plus', function() {
+	$j(document).on( 'click', '.quantity .plus, .quantity .minus, .product-thumbnail .img-plus, .box-container .slot', function() {
 
 		// Get values
 									// Find the form input value from outside it self container
 		var $qty		= $j(this).parent().parent('tr').find('.product-quantity .quantity .qty'),
+			$boxQty 		= $j(this).parent().parent().parent().parent().find('.summary form .mnm_item td.mnm-376 .quantity .qty'),
 			currentVal	= parseFloat( $qty.val() ),
 			max			= parseFloat( $qty.attr( 'max' ) ),
 			min			= parseFloat( $qty.attr( 'min' ) ),
-			step		= $qty.attr( 'step' );
+			step		= $qty.attr( 'step' ),
+
+			boxCurrentVal	= parseFloat( $boxQty.val() ),
+			boxMax			= parseFloat( $boxQty.attr( 'max' ) ),
+			boxMin			= parseFloat( $boxQty.attr( 'min' ) ),
+			boxStep		= $boxQty.attr( 'step' );
 
 		// Format values
 		if ( ! currentVal || currentVal === '' || currentVal === 'NaN' ) currentVal = 0;
@@ -56,64 +62,86 @@ function initAddToCartPlusMinus(){
 		}
 
 		// *** Change the value on product image click *** //
+		// With tag img
+		if ( $j( this ).is( 'img' ) ) {
 
-		// With class .img-plus
-		if ( $j( this ).is( '.img-plus' ) ) {
 			console.log('plus cicked image');
 
 			if ( max && ( max == currentVal || currentVal > max ) ) {
 				$qty.val( max );
+
 			} else {
 				//  Add the quanity to the form value so it can be updated
 				// to the cart
 				$qty.val( currentVal + parseFloat( step ) );
-				console.log($qty);
 			}
 
-		}
+		} else if( $j(this).is('.mnm-376') ){
+			console.log('minus cicked image');
+			if ( max && ( max == currentVal || currentVal > max ) ) {
+				$qty.val( max );
+			} else if ( boxCurrentVal > 0 ) {
+				$boxQty.val( boxCurrentVal - parseFloat( step ) );
+			}
+
+			// REMOVE IMAGE
+			// 1 .Fetch Class marked Draggable
+			var name = $j(this).parent('.slot')      //.find('.slot');
+
+			// 2 .Check if class exists on click
+			if(name.length != 0) {
+				console.log('removed');
+        		// 3 .Remove draggable image and replace with default
+        			$j(name).find("div").remove();
+        			$j(name).removeClass('containsMac');
+        			$j(name).addClass('empty');
+				$j('<img src="http://tiktalkinteractive.com/savor/wp-content/uploads/2017/03/MacLogo_Picker.png" alt="Macaron" class="ui-droppable">').appendTo(jQuery(name));
+
+			}
+		} // End if
 
 		// Trigger change event
 		$qty.trigger( 'change' );
 
 	}); // END CLICK TO CART FUNCTION
 
-$j(document).on('click', "#left-pane .ui-draggable", function(){
 
-			var macaronCount = $j(".showBox .containsMac").length;
-			var totalBoxMacs = $j(".slot").length
+	// *** ADD MACARON TO BASKET ON CLICK *** //
+	$j(document).on('click', "#left-pane .ui-draggable", function(){
 
-
-
-			if (macaronCount < totalBoxMacs) {
-
-				// 1 .Search through for empty basket item
-				for (var n = 1; n < 19; ++ n){
-
-				var macaron = '.showBox #slot-' + n;
+				var macaronCount = $j(".showBox .containsMac").length;
+				var totalBoxMacs = $j(".slot").length
 
 
-					// 2. If empty replace with clicked macaron
-					if ($j(macaron).hasClass("empty")) {
 
-						$j(macaron).children("img").remove();
-								$j(this).clone().appendTo(macaron).css({"transform":"rotate(-90deg)", "width":"100px"} );
-								$j(macaron).removeClass("empty");
-								$j(macaron).addClass("containsMac");
+				if (macaronCount < totalBoxMacs) {
 
-						//Count Macarons and show add button when full
-							var totalBasketMacs = $j(".showBox .slot").length
-							var macaronsAdded = $(".showBox .containsMac").length;
+					// 1 .Search through for empty basket item
+					for (var n = 1; n < 19; ++ n){
 
-							if (macaronsAdded == totalBasketMacs) {
-					$j(this).MacaronPickerResults();
-							}
-
-								return false;
-							}
-				}
-			 }
-	});
+					var macaron = '.showBox #slot-' + n;
 
 
+						// 2. If empty replace with clicked macaron
+						if ($j(macaron).hasClass("empty")) {
+
+							$j(macaron).children("img").remove();
+									$j(this).clone().appendTo(macaron).css({"transform":"rotate(-90deg)", "width":"100px"} );
+									$j(macaron).removeClass("empty");
+									$j(macaron).addClass("containsMac");
+
+							//Count Macarons and show add button when full
+								var totalBasketMacs = $j(".showBox .slot").length
+								var macaronsAdded = $(".showBox .containsMac").length;
+
+								if (macaronsAdded == totalBasketMacs) {
+						$j(this).MacaronPickerResults();
+								}
+
+									return false;
+								}
+					}
+				 }
+		});
 
 }
--- templates/single-product/mnm/mnm-product-thumbnail.php
+++ templates/single-product/mnm/mnm-product-thumbnail.php
@@ -9,6 +9,6 @@ if ( ! defined( 'ABSPATH' ) ){
 }
 ?>
 
-<div class="mnm_image ui-draggable plus">
+<div class="mnm_image ui-draggable plus mnm-<?php echo $mnm_product->variation_id ? $mnm_product->variation_id : $mnm_product->id; ?>" >
 	<?php echo $mnm_product->get_image( apply_filters( 'woocommerce_mnm_product_thumbnail_size', 'shop_thumbnail' ) ); ?>
 </div>
